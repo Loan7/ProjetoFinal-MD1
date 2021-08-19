d8b30c145ce690edc2338e51a9f3bdb83db4a9cd
Loan7 <olivera_rox@hotmail.com>
2021-08-19 00:59:06 -0300

    iniciando no javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function primeiraEscolha(){
+    while(questao !== 1 ||questao !== 2){
+
+    var questao = prompt("Escolha uma das opções");
+    if(questao == 1){
+            location.replace("luffy-stage2.html");
+            break
+        }
+    }
+}
